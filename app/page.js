1699f94df360b08a6746a46d52ef1915f577cb78
christapi2099 <94759708+christapi2099@users.noreply.github.com>
2024-08-04 21:04:14 -0700

    Spacing

--- app/page.js
+++ app/page.js
@@ -165,9 +165,11 @@ export default function Home(){
                   <Typography variant={'h3'} color={'#333'} textAlign={'center'}>
                     Quantity: {quantity}
                   </Typography>
+                  
                   <Button variant="contained" onClick={() => addItem(name)}>
                     Add
                   </Button>
+
                   <Button variant="contained" onClick={() => removeItem(name)}>
                     Remove
                   </Button>
